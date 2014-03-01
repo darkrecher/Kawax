17ec88266e31567e1a302a94b3db3860162b65d3
darkrecher <recher_burp@yahoo.fr>
2014-03-01 10:39:51 +0100

    Tutoriel touillette. Ajout texte anglais.

--- code/gamemode/touytuto.py
+++ code/gamemode/touytuto.py
@@ -8,6 +8,7 @@ Repo : https://github.com/darkrecher/Kawax
 
 from common   import (pyRect, pyRectTuple,
                       UP, DOWN, LEFT, RIGHT, NO_SOUND)
+from language import LANGUAGE_FRENCH, LANGUAGE_ENGLISH
 
 from console  import Console
 from coins    import ChipCoin, ChipSugar
@@ -25,82 +26,163 @@ LIST_TUT_STEP_DESCRIP = (
         STEP_COND_STIM,
         (),
         u"tuto_02_01",
-        (u"Bienvenue dans le second",
-         u"tutoriel de Kawax. ",
-         u"Dans ce mode, vous",
-         u"devez faire tomber des",
-         u"touillettes à café",
-         u"en bas de l'aire de jeu."),
+        {
+            LANGUAGE_FRENCH : (
+                u"Bienvenue dans le second",
+                u"tutoriel de Kawax. ",
+                u"Dans ce mode, vous",
+                u"devez faire tomber des",
+                u"touillettes à café",
+                u"en bas de l'aire de jeu.",
+            ),
+            LANGUAGE_ENGLISH : (
+                u"Welcome to the second",
+                u"Kawax tutorial.",
+                u"In this mode, you have",
+                u"to move some coffee",
+                u"spoons to the bottom",
+                u"of the screen.",
+            )
+        },
         (),
         False
     ),(
         STEP_COND_STIM,
         (),
         u"tuto_02_02",
-        (u"Bon ...",
-         u"J'en ai marre de faire",
-         u"ma voix pseudo-sexy.",
-         u"Alors vous allez faire",
-         u"ce que je dis,",
-         u"sans pinailler. Ok ?"),
+        {
+            LANGUAGE_FRENCH : (
+                u"Bon ...",
+                u"J'en ai marre de faire",
+                u"ma voix pseudo-sexy.",
+                u"Alors vous allez faire",
+                u"ce que je dis,",
+                u"sans pinailler. Ok ?",
+            ),
+            LANGUAGE_ENGLISH : (
+                u"Well...",
+                u"I'm fed up with",
+                u"making my",
+                u"pseudo-sexy voice.",
+                u"So, you will just do",
+                u"what I tell you, OK ?",
+            )
+        },
         (),
         False
     ),(
         STEP_COND_SELECT_TILES,
         ((5, 7), (6, 7), (7, 7), (8, 7), (9, 7), (5, 8), (6, 8)),
         u"tuto_02_03",
-        (u"Les cases, là, ",
-         u"vous les sélectionnez."),
+        {
+            LANGUAGE_FRENCH : (
+                u"Les cases, là, ",
+                u"vous les sélectionnez.",
+            ),
+            LANGUAGE_ENGLISH : (
+                u"Select the",
+                u"blink-blinking tiles.",
+            )
+        },
         ((5, 7), (6, 7), (7, 7), (8, 7), (9, 7), (5, 8), (6, 8)),
         False
     ),(
         STEP_COND_STIM,
         (),
         u"tuto_02_04",
-        (u"Wouhouuuu !",
-         u"Regardez la touillette,",
-         u"elle est descendue."),
+        {
+            LANGUAGE_FRENCH : (
+               (u"Wouhouuuu !",
+                u"Regardez la touillette,",
+                u"elle est descendue."),
+            ),
+            LANGUAGE_ENGLISH : (
+                u"Woohooo !",
+                u"Look at the spoon,",
+                u"it went down.",
+            )
+        },
         (),
         False
     ),(
         STEP_COND_SELECT_TILES,
         ((7, 8), (8, 8), (9, 8), (5, 9), (6, 9), (7, 9), (8, 9), (9, 9)),
         u"tuto_02_05",
-        (u"Encore un petit effort,",
-         u"feignasse de stagiaire."),
+        {
+            LANGUAGE_FRENCH : (
+               (u"Encore un petit effort,",
+                u"feignasse de stagiaire."),
+            ),
+            LANGUAGE_ENGLISH : (
+                u"One more effort,",
+                u"you lazy trainee.",
+            )
+        },
         ((7, 8), (8, 8), (9, 8), (5, 9), (6, 9), (7, 9), (8, 9), (9, 9)),
         False
     ),(
         STEP_COND_STIM,
         (),
         u"tuto_02_06",
-        (u"Et crac ! La touillette ",
-         u"a disparue.",
-         u"C'est top foufoutre. "),
+        {
+            LANGUAGE_FRENCH : (
+               (u"Et crac ! La touillette ",
+                u"a disparue.",
+                u"C'est top foufoutre. "),
+            ),
+            LANGUAGE_ENGLISH : (
+                u"Bing ! the spoon",
+                u"disappeared. This is",
+                u"so dibbly-didum-cool.",
+            )
+        },
         (),
         False
     ),(
         STEP_COND_STIM,
         (),
         u"tuto_02_07",
-        (u"C'est génial. Et sinon,",
-         u"quand vous supprimez ",
-         u"des gros tas de pièces,",
-         u"d'autre touillettes",
-         u"peuvent apparaitre",
-         u"en haut de l'écran."),
+        {
+            LANGUAGE_FRENCH : (
+                u"C'est génial. Et sinon,",
+                u"quand vous supprimez ",
+                u"des gros tas de pièces,",
+                u"d'autre touillettes",
+                u"peuvent apparaitre",
+                u"en haut de l'écran.",
+            ),
+            LANGUAGE_ENGLISH : (
+                u"Yeah, fun. Oh, and",
+                u"when you remove a",
+                u"big bunch of coins,",
+                u"other spoons can",
+                u"appear at the top",
+                u"of the screen.",
+            )
+        },
         (),
         False
     ),(
         STEP_COND_STIM,
         (),
         u"tuto_02_08",
-        (u"Faut que j'y aille,",
-         u"j'ai une réunion avec ",
-         u"d'autres morceaux de ",
-         u"mon cerveau.",
-         u"Eclate-toi bien",
-         u"jeune boulawan !"),
+        {
+            LANGUAGE_FRENCH : (
+               (u"Faut que j'y aille,",
+                u"j'ai une réunion avec ",
+                u"d'autres morceaux de ",
+                u"mon cerveau.",
+                u"Eclate-toi bien",
+                u"jeune boulawan !"),
+            ),
+            LANGUAGE_ENGLISH : (
+                u"I have to go, I got",
+                u"a meeting with other",
+                u"parts of my brain.",
+                u"Have fun, young",
+                u"Shitwalker !",
+            )
+        },
         (),
         False
     ),(
